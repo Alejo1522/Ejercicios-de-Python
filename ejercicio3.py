28a4db21a695c33543ce4b1cbc4f866bf1e4008b
Alejandro <alejotrujillo146@gmail.com>
2023-12-22 15:31:10 -0500

    Correccíon de un promedio erroneo

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,10 +1,10 @@
 #Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
-
-def promedio(notas):
-    return (f"El promeido es de {notas/4}")
+nota=0
+def promedio(nota):
+    return (f"El promeido es de {nota/4}")
 
 for i in range(4):
     notas=float(input(f"Digite la nota {i+1}: "))
-    notas+=notas
+    nota=nota+notas
 
-print(promedio(notas))
+print(promedio(nota))
