d1404eb11d6dd55a3f71c5dc4496ff2527e099b7
Alejandro <alejotrujillo146@gmail.com>
2023-12-26 12:27:44 -0500

    Ejercicio #4 ciclos

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,19 +1,18 @@
-#Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es.
+# Escriba un programa que muestre una tabla de multiplicar como la siguiente:
+#  1   2   3   4   5   6   7   8   9  10
+#  2   4   6   8  10  12  14  16  18  20
+#  3   6   9  12  15  18  21  24  27  30
+#  4   8  12  16  20  24  28  32  36  40
+#  5  10  15  20  25  30  35  40  45  50
+#  6  12  18  24  30  36  42  48  54  60
+#  7  14  21  28  35  42  49  56  63  70
+#  8  16  24  32  40  48  56  64  72  80
+#  9  18  27  36  45  54  63  72  81  90
+# 10  20  30  40  50  60  70  80  90 100
+# Los números deben estar alineados a la derecha.
 
-def convercion(palabra_1, palabra_2):
-    longitud1=len(palabra_1)
-    longitud2=len(palabra_2)
-
-    if longitud1 > longitud2:
-        cantidad_letras=longitud1 - longitud2
-        return (f"La palabra {palabra_1} tiene {cantidad_letras} letras mas que {palabra_2}.")
-    elif longitud1 < longitud2:
-        cantidad_letras=longitud2 - longitud1
-        return (f"La palabra {palabra_2} tiene  {cantidad_letras} letras mas que {palabra_1}.")
-    else:
-        return ("Las dos palabras tienen el mismo largo")
-    
-palabra_1=input("Ingrese una palabra: ")
-palabra_2=input("Ingrese otra palabra: ")
-
-print(convercion(palabra_1, palabra_2))
+for i in range(1,11):
+    for k in range(1,11):
+        multipicacion=i*k
+        print(f"{multipicacion:2}", end=" ")
+    print()
