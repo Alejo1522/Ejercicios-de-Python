effa31936820e4781e36b9e0ae2e63bc4941c22c
Alejandro <alejotrujillo146@gmail.com>
2023-12-26 13:30:04 -0500

    Ejercicio #3 patrones comunes

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,15 +1,11 @@
-# Un jugador debe lanzar dos dados numerados de 1 a 6, y su puntaje es la suma de los valores obtenidos.
-# Un puntaje dado puede ser obtenido con varias combinaciones posibles. Por ejemplo, el puntaje 4 se logra con las siguientes tres combinaciones: 1+3, 2+2 y 3+1.
-# # Escriba un programa que pregunte al usuario un puntaje, y muestre como resultado la cantidad de combinaciones de dados con las que se puede obtener ese puntaje
+# Escriba un programa que permita determinar el número mayor perteneciente a un conjunto de n números, donde tanto el valor de n como el de los números deben ser ingresados por el usuario.
 
-numero=int(input("Ingrese el puntaje : "))
+cantidad=int(input("Cuantos números desea ingresar?: "))
 
-if numero<12:
-    cantidad=0
-    for i in range(1,7):
-        for k in range(1,7):
-            if i + k == numero:
-                cantidad+=1
-    print(f"Hay {cantidad} combinaciones para obtener {numero}")
-else:
-    print(f"Hay 0 combinaciones para obtener {numero}")
+for i in range(1, cantidad+1):
+    numero=int(input("Ingrese un número: "))
+    mayor=numero
+    if mayor<numero:
+        mayor=numero
+
+print(f"El mayor es {mayor}")
