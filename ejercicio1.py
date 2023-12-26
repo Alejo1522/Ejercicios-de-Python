b282a36285521b9301e392321d3737357ffeeb87
Alejandro <alejotrujillo146@gmail.com>
2023-12-26 14:20:44 -0500

    Ejercicio #1 Algoritmos

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,7 @@
-# Escriba un programa que muestre los números naturales menores o iguales que un número n determinado, que no sean múltiplos ni de 3 ni de 7.
+# Escriba un programa que determine la cantidad de dígitos en un número entero ingresado por el usuario:
 
 numero=int(input("Ingrese un numero: "))
 
-for i in range(1, numero+1):
-    if i % 3 != 0 and i % 7 != 0:
-        print(i, end=" ")
+cantidad=len(str(abs(numero)))
+
+print(f"El número {numero} tiene {cantidad} de digitos")
