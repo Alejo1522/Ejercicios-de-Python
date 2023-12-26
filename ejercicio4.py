10475cf342ab52d7aa9ecd0d1369f9532591ad8b
Alejandro <alejotrujillo146@gmail.com>
2023-12-26 16:04:56 -0500

    Ejercicio #4 Algoritmos

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,15 +1,39 @@
-# Un jugador debe lanzar dos dados numerados de 1 a 6, y su puntaje es la suma de los valores obtenidos.
-# Un puntaje dado puede ser obtenido con varias combinaciones posibles. Por ejemplo, el puntaje 4 se logra con las siguientes tres combinaciones: 1+3, 2+2 y 3+1.
-# # Escriba un programa que pregunte al usuario un puntaje, y muestre como resultado la cantidad de combinaciones de dados con las que se puede obtener ese puntaje
+# En cada ronda del juego del cachipún, los dos competidores deben elegir entre jugar tijera, papel o piedra.
 
-numero=int(input("Ingrese el puntaje : "))
+# Las reglas para decidir quién gana la ronda son: tijera le gana a papel, papel le gana a piedra, piedra le gana a tijera, y todas las demás combinaciones son empates.
 
-if numero<12:
-    cantidad=0
-    for i in range(1,7):
-        for k in range(1,7):
-            if i + k == numero:
-                cantidad+=1
-    print(f"Hay {cantidad} combinaciones para obtener {numero}")
-else:
-    print(f"Hay 0 combinaciones para obtener {numero}")
+# El ganador del juego es el primero que gane tres rondas.
+
+# Escriba un programa que pregunte a cada jugador cuál es su jugada, muestre cuál es el marcador después de cada ronda, y termine cuando uno de ellos haya ganado tres rondas. Los jugadores deben indicar su jugada escribiendo tijera, papel o piedra.
+puntajeA=0
+puntajeB=0
+def jugar_ronda(jugadorA, jugadorB):
+    if jugadorA==jugadorB:
+        return 0
+    elif(jugadorA == "piedra" and jugadorB == "tijera") or (jugadorA == "papel" and jugadorB == "piedra") or (jugadorA == "tijera" and jugadorB == "papel"):
+        return (f"{puntajeA+1} - {puntajeB}")
+    
+    else:
+        return (f"{puntajeA} - {puntajeB+1}")
+        
+def jugar_juego():
+    rondas=3
+
+    jugadores=["jugadorA", "jugadorB"]
+    opciones=["piedra","tijera","papel"]
+
+    for ronda in range(1, rondas+1):
+        print(f"Ronda: {ronda}")
+
+        for jugador in jugadores:
+            seleccion=input(f"{jugador} elige una opcion piedra, papel o tijera: ").lower()
+
+            while seleccion not in opciones:
+                print("¡Opción no valida! Debes elegir piedra, papel o tijera")
+                seleccion=input(f"{jugador} elige una opcion piedra, papel o tijera: ").lower()
+
+        resultado=jugar_ronda(jugadores[0], jugadores[1])
+        print(f"{resultado}")
+        
+
+jugar_juego()
