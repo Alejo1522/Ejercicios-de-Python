579ec55a6cd936839d8a2f4b922430a4730c6d81
Alejandro <alejotrujillo146@gmail.com>
2023-12-26 13:09:02 -0500

    Ejercicio #2 patrones_comunes

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,22 @@
-# Escriba un programa que genere todas las potencias de 2, desde la 0-ésima hasta la ingresada por el usuario:
+# Escriba un programa que entregue la suma de los primeros n números naturales, siendo n ingresado por el usuario.
+# Matemáticamente lo que se pide que haga el programa es realizar la siguiente sumatoria.
+# S1=∑i=1ni=1+2+3+4+5+6+⋯+n
+# Además, obtenga el resultado de la siguiente fórmula.
+# S2n×(n+1)/2
+# El programa debe entregar el resultado diciendo si S1 y S2 son iguales o no.
 
-numero=int(input("Ingrese un numero: "))
+numero=int(input("Ingrese un número: "))
 
-for i in range(0, numero+1):
-    potencia=2**i
-    print(potencia, end=" ")
+for i in range(1, numero+1):
+    S1=i
+    S1+=i
+
+S2=(numero*(numero+1))//2
+
+print(f"S1:{S1}")
+print(f"S2:{S2}")
+
+if S1 == S2:
+    print("Son iguales")
+else:
+    print("No son iguales")
