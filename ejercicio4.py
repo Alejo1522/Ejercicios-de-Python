7316812b5a20f2ddd3586de99355e709bb4062c8
Alejandro <alejotrujillo146@gmail.com>
2023-12-26 13:30:16 -0500

    Ejercicio #4 patrones comunes

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,18 +1,15 @@
-# Escriba un programa que muestre una tabla de multiplicar como la siguiente:
-#  1   2   3   4   5   6   7   8   9  10
-#  2   4   6   8  10  12  14  16  18  20
-#  3   6   9  12  15  18  21  24  27  30
-#  4   8  12  16  20  24  28  32  36  40
-#  5  10  15  20  25  30  35  40  45  50
-#  6  12  18  24  30  36  42  48  54  60
-#  7  14  21  28  35  42  49  56  63  70
-#  8  16  24  32  40  48  56  64  72  80
-#  9  18  27  36  45  54  63  72  81  90
-# 10  20  30  40  50  60  70  80  90 100
-# Los números deben estar alineados a la derecha.
+# Un jugador debe lanzar dos dados numerados de 1 a 6, y su puntaje es la suma de los valores obtenidos.
+# Un puntaje dado puede ser obtenido con varias combinaciones posibles. Por ejemplo, el puntaje 4 se logra con las siguientes tres combinaciones: 1+3, 2+2 y 3+1.
+# # Escriba un programa que pregunte al usuario un puntaje, y muestre como resultado la cantidad de combinaciones de dados con las que se puede obtener ese puntaje
 
-for i in range(1,11):
-    for k in range(1,11):
-        multipicacion=i*k
-        print(f"{multipicacion:2}", end=" ")
-    print()
+numero=int(input("Ingrese el puntaje : "))
+
+if numero<12:
+    cantidad=0
+    for i in range(1,7):
+        for k in range(1,7):
+            if i + k == numero:
+                cantidad+=1
+    print(f"Hay {cantidad} combinaciones para obtener {numero}")
+else:
+    print(f"Hay 0 combinaciones para obtener {numero}")
