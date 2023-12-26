f088cac9371f073070b88d4aff2159f9d4a9faf5
Alejandro <alejotrujillo146@gmail.com>
2023-12-26 11:18:31 -0500

    Ejercicio #2 ciclos

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,11 +1,7 @@
-# Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
+# Escriba un programa que genere todas las potencias de 2, desde la 0-ésima hasta la ingresada por el usuario:
 
-def es_bisiesto(year):
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        return (f"{year} es bisiesto")
-    else:
-        return (f"{year} no es bisiesto")
+numero=int(input("Ingrese un numero: "))
 
-year=int(input("Ingrese un año: "))
-
-print(es_bisiesto(year))
+for i in range(0, numero+1):
+    potencia=2**i
+    print(potencia, end=" ")
